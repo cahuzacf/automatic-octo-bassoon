8db44e69d2a71c21d0a1b913a7e44bf9d523e7d7
ts_ci <ts_ci@github.com>
2020-05-16 06:23:32 +0000

    Moving dist files to root for shorter import statements.

---
+++ myFunction.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.myFunction = void 0;
+exports.myFunction = function () { return Promise.resolve(["a", "b", "c"]); };
+//# sourceMappingURL=myFunction.js.map
---
+++ zz_esm/myFunction.js
@@ -0,0 +1,2 @@
+export var myFunction = function () { return Promise.resolve(["a", "b", "c"]); };
+//# sourceMappingURL=myFunction.js.map
